8aca095a274c999ce82d5fa75e711fd60536ee18
NetworkMonk <jamesplant@gmail.com>
2024-07-02 21:30:37 +0100

    Fetch skills from hygraph

--- src/components/sections/Technologies.js
+++ src/components/sections/Technologies.js
@@ -3,39 +3,34 @@ import Section from "@/components/common/Section";
 import { josefin } from "@/app/fonts";
 import InView from "../animated/InView";
 
-export default function Technologies() {
-  const skillsList = [
-    "JavaScript",
-    "React",
-    "Next.js",
-    "Node.js",
-    "Tailwind",
-    "Framer Motion",
-    "Bootstrap",
-    "Electron",
-    "Python",
-    "PHP",
-    "Laravel",
-    "Docker",
-    "MySQL",
-    "HTML",
-    "CSS",
-    "Sass",
-    "CI/CD",
-    "NetSuite",
-    "Suite Script",
-    "Celigo",
-    "REST API",
-    "Postman",
-    "Webflow",
-    "Squarespace",
-    "WordPress",
-    "Shopify",
-    "Magento",
-    "C#",
-    "Unity",
-    "Go",
-  ];
+const getTechnologies = async () => {
+  const response = await fetch(
+    "https://api-eu-west-2.hygraph.com/v2/cly38j74i00f307w7tb295z6r/master",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      next: { revalidate: 3600 },
+      body: JSON.stringify({
+        query: `{
+          technologies(first: 100) {
+            displayOrder
+            name
+          }
+        }
+      `,
+      }),
+    }
+  );
+
+  const { data } = await response.json();
+  return data.technologies;
+};
+
+export default async function Technologies() {
+  const technologies = await getTechnologies();
 
   return (
     <div id="technologies">
@@ -60,18 +55,20 @@ export default function Technologies() {
                 too much to list in a meaningful way!
               </p>
               <div className="flex flex-wrap gap-4 mt-10">
-                {skillsList.map((skill, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="bg-brand-platinum bg-opacity-15 hover:bg-opacity-100 text-gray-100 hover:text-gray-900 rounded p-3 text-sm font-medium tracking-widest cursor-default transition-colors duration-300 group in-up"
-                      style={{ animationDelay: `${index / 6 + 2}s` }}
-                    >
-                      <span className="inline-block rounded-full h-2.5 w-2.5 mr-2.5 bg-teal-400 group-hover:bg-brand-orange transition-colors duration-300"></span>
-                      {skill}
-                    </div>
-                  );
-                })}
+                {technologies
+                  .sort((a, b) => a.displayOrder - b.displayOrder)
+                  .map((skill, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="bg-brand-platinum bg-opacity-15 hover:bg-opacity-100 text-gray-100 hover:text-gray-900 rounded p-3 text-sm font-medium tracking-widest cursor-default transition-colors duration-300 group in-up"
+                        style={{ animationDelay: `${index / 6 + 2}s` }}
+                      >
+                        <span className="inline-block rounded-full h-2.5 w-2.5 mr-2.5 bg-teal-400 group-hover:bg-brand-orange transition-colors duration-300"></span>
+                        {skill.name}
+                      </div>
+                    );
+                  })}
               </div>
             </div>
           </InView>
